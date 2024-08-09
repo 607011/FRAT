8232dc243d46d88fd3b34094f00b4f0b6f2e15c0
Oliver Lau <oliver@ersatzworld.net>
2024-08-09 17:56:19 +0200

    Refactorings.

--- checker++/checker.cpp
+++ checker++/checker.cpp
@@ -1,42 +1,42 @@
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include <boost/multiprecision/cpp_int.hpp>
 
 namespace mp = boost::multiprecision;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-  std::string f = "fibonacci.txt";
-  std::cout << "Reading " << f << " ... " << std::flush;
-  std::ifstream in(f);
+  std::string filename = "fibonacci.txt";
+  std::cout << "Reading " << filename << " ... " << std::flush;
+  std::ifstream in(filename);
   if (!in) {
-    std::cerr << "Error opening file" << std::endl;
-    return 1;
+    std::cerr << "Error opening file '" << filename << "'" << std::endl;
+    return EXIT_FAILURE;
   }
-
-  mp::cpp_int p = 0, n = 1;
-  size_t c = 0;
-  std::string s;
-  while (std::getline(in, s)) {
+  mp::cpp_int prev = 0;
+  mp::cpp_int next = 1;
+  std::size_t line_no = 0;
+  std::string line;
+  while (std::getline(in, line)) {
     try {
-      mp::cpp_int i = boost::lexical_cast<mp::cpp_int>(s);
-      if (i != p) {
-        std::cerr << "ERROR: Wrong number at line " << c + 1 << std::endl;
-        return 1;
+      mp::cpp_int current = boost::lexical_cast<mp::cpp_int>(line);
+      if (current != prev) {
+        std::cerr << "ERROR: Wrong number at line " << (line_no + 1) << std::endl;
+        return EXIT_FAILURE;
       }
-      mp::cpp_int t = n;
-      n += p;
-      p = t;
-      ++c;
-      if (c % 10000 == 0) {
-        std::cout << "\rReading " << f << " ... " << c << std::flush;
+      mp::cpp_int tmp = next;
+      next += prev;
+      prev = tmp;
+      if (++line_no % 10000 == 0) {
+        std::cout << "\rReading " << filename << " ... " << line_no << std::flush;
       }
-    } catch (...) {
-      std::cerr << "Error parsing number" << std::endl;
-      return 1;
+    } catch (const boost::bad_lexical_cast& e) {
+      std::cerr << "Error parsing number in line " << (line_no + 1) << ": " << e.what() << std::endl;
+      return EXIT_FAILURE;
     }
   }
-  std::cout << "\nSUCCESS: " << c << " Fibonacci numbers verified." << std::endl;
-  return 0;
+  std::cout << "\nSUCCESS: " << line_no << " Fibonacci numbers verified." << std::endl;
+  return EXIT_SUCCESS;
 }
